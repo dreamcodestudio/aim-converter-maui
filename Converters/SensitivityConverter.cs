2ad697ee349590470350a75a48de5ed530aac902
dreamcodestudio <>
2023-11-25 20:32:17 +0300

    feat : Add game Sensitivity converter

---
+++ Converters/SensitivityConverter.cs
@@ -0,0 +1,12 @@
+﻿using AimConverter.Models;
+
+namespace AimConverter.Converters
+{
+    internal class SensitivityConverter
+    {
+        public static float Convert(float value, GameInfo from, GameInfo to)
+        {
+            return value * from.Sensitivity / to.Sensitivity;
+        }
+    }
+}
