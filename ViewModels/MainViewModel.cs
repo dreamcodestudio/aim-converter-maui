c5326e48cafe2591ce2a88626b9a991958066230
dreamcodestudio <>
2023-12-02 19:57:49 +0300

    feat: Handle GameInfo changed events

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -10,14 +10,42 @@ namespace AimConverter.ViewModels
         public IReadOnlyList<GameInfo> Games => AppData.Games;
         [ObservableProperty] private string _targetSens = null!;
 
+        private float _sourceSens;
+        private GameInfo? _sourceGame;
+        private GameInfo? _targetGame;
+
         [RelayCommand]
-        public void OnSourceSensChanged(string value)
+        private void OnSourceGameChanged(GameInfo gameInfo)
+        {
+            _sourceGame = gameInfo;
+            TargetSens = _sourceGame.Name;
+            Refresh();
+        }
+
+        [RelayCommand]
+        private void OnTargetGameChanged(GameInfo gameInfo)
+        {
+            _targetGame = gameInfo;
+            TargetSens = _targetGame.Name;
+            Refresh();
+        }
+
+        [RelayCommand]
+        private void OnSourceSensChanged(string value)
         {
             if (string.IsNullOrWhiteSpace(value))
                 return;
             if (!float.TryParse(value, out var sourceSens))
                 return;
-            var convertedValue = SensitivityConverter.Convert(sourceSens, Games[0], Games[1]);
+            _sourceSens = sourceSens;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (_sourceGame == null || _targetGame == null)
+                return;
+            var convertedValue = SensitivityConverter.Convert(_sourceSens, _sourceGame, _targetGame);
             const string sensFormat = "0.000";
             TargetSens = convertedValue.ToString(sensFormat);
         }
